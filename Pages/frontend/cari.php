8bc7699ff4a6827ef44e15bd959c319ef2d34f0e
rafif1234 <57853129+rafif1234@users.noreply.github.com>
2019-11-28 11:57:28 +0700

    cari
    tampilan

---
+++ Pages/frontend/cari.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>INFOTANI - CARI DATA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="./css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/cari.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <form class="form-signin" action="proses_login.php" method="post">
+        <br> 
+        <h2 class="form-signin-heading" align="center" style="color: black;">CARI DATA INFOTANI</h2>
+        
+  <input class="search"  type="text" placeholder="Cari..." required> 
+  <input class="button"  type="button" value="Cari">    
+  </form>
+  <style type="text/css"0>
+.search {
+    padding:8px 15px;
+    width: 500px;
+    background:rgba(50, 50, 50, 0.5);
+    border:0px solid #dbdbdb;
+}
+.button {
+    position:relative;
+    padding:6px 15px;
+    left:-8px;
+    border:2px solid #53bd84;
+    background-color:#53bd84;
+    color:#fafafa;
+}
+</style> 
+
+    </div> <!-- /container -->
+
+     <script>window.jQuery || document.write('<script src="./assets/jquery.min.js"><\/script>')</script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <script type="text/javascript">
+      $(document).ready(function(){
+        setTimeout(function(){
+          $(".alert").slideUp();
+        },3000);
+      });
+    </script>
+    
+  </body>
+</html>
