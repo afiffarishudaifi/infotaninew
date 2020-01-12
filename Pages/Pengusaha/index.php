2261ece618c3a4365090e0459d2a078c9b6ef1d0
rafif1234 <57853129+rafif1234@users.noreply.github.com>
2020-01-12 16:46:58 +0700

    dashboard pengusaha

--- Pages/Pengusaha/index.php
+++ Pages/Pengusaha/index.php
@@ -43,91 +43,82 @@
       <!-- Small boxes (Stat box) -->
       <div class="row">
         <div class="col-lg-12">
-          <?php
-            $cekdata = "SELECT * FROM perusahaan WHERE perusahaan.ID_PERUSAHAAN='$id_pengguna' AND perusahaan.NAMA_PERUSAHAAN=''";
-            $querydata = mysqli_query($koneksi, $cekdata);
-            $hasilcekdata=mysqli_fetch_array($querydata);
-            if($hasilcekdata!=null) {
-                ?>
-                  <script language="JavaScript">
-                  alert('Lengkapi Data !');
-                  setTimeout(function() {window.location.href='./tambahdata.php'},10);
-                  </script>
-                <?php
-            }
-
-            $query1 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 01 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+         <?php
+include "../../controller/admin/koneksi.php";
+         $query1 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan= $id_pengguna AND month(TANGGAL) = 01 and year(TANGGAL) = $tahun";
             $sql1 = mysqli_query($koneksi, $query1);
             while ($data1 = mysqli_fetch_row($sql1)) {
                 $id1 = $data1[0];
             }
 
-            $query2 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 02 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query2 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 02 and year(TANGGAL) = $tahun";
             $sql2 = mysqli_query($koneksi, $query2);
             while ($data2 = mysqli_fetch_row($sql2)) {
                 $id2 = $data2[0];
             }
 
-            $query3 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 03 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query3 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 03 and year(TANGGAL) = $tahun";
             $sql3 = mysqli_query($koneksi, $query3);
             while ($data3 = mysqli_fetch_row($sql3)) {
                 $id3 = $data3[0];
             }
 
-            $query4 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 04 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query4 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 04 and year(TANGGAL) = $tahun";
             $sql4 = mysqli_query($koneksi, $query4);
             while ($data4 = mysqli_fetch_row($sql4)) {
                 $id4 = $data4[0];
             }
 
-            $query5 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 05 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query5 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 05 and year(TANGGAL) = $tahun";
             $sql5 = mysqli_query($koneksi, $query5);
             while ($data5 = mysqli_fetch_row($sql5)) {
                 $id5 = $data5[0];
             }
 
-            $query6 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 06 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query6 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 06 and year(TANGGAL) = $tahun";
             $sql6 = mysqli_query($koneksi, $query6);
             while ($data6 = mysqli_fetch_row($sql6)) {
                 $id6 = $data6[0];
             }
 
-            $query7 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 07 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query7 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 07 and year(TANGGAL) = $tahun";
             $sql7 = mysqli_query($koneksi, $query7);
             while ($data7 = mysqli_fetch_row($sql7)) {
                 $id7 = $data7[0];
             }
 
-            $query8 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 08 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query8 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 08 and year(TANGGAL) = $tahun";
             $sql8 = mysqli_query($koneksi, $query8);
             while ($data8 = mysqli_fetch_row($sql8)) {
                 $id8 = $data8[0];
             }
 
-            $query9 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 09 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query9 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 09 and year(TANGGAL) = $tahun";
             $sql9 = mysqli_query($koneksi, $query9);
             while ($data9 = mysqli_fetch_row($sql9)) {
                 $id9 = $data9[0];
             }
 
-            $query10 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 10 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query10 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 10 and year(TANGGAL) = $tahun";
             $sql10 = mysqli_query($koneksi, $query10);
             while ($data10 = mysqli_fetch_row($sql10)) {
                 $id10 = $data10[0];
             }
 
-            $query11 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 11 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query11 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 11 and year(TANGGAL) = $tahun";
             $sql11 = mysqli_query($koneksi, $query11);
             while ($data11 = mysqli_fetch_row($sql11)) {
                 $id11 = $data11[0];
             }
 
-            $query12 = "SELECT SUM(pemesanan.JUMLAH_PESAN) AS TOTAL FROM pemesanan WHERE pemesanan.ID_PERUSAHAAN= '$id_pengguna' AND AND month(TANGGAL) = 12 and year(TANGGAL) = $tahun and ID_PESAN_STATUS=2";
+            $query12 = "SELECT count(*) FROM pemesanan WHERE id_perusahaan = $id_pengguna AND month(TANGGAL) = 12 and year(TANGGAL) = $tahun";
             $sql12 = mysqli_query($koneksi, $query12);
             while ($data12 = mysqli_fetch_row($sql12)) {
                 $id12 = $data12[0];
             }
-            
+
+
+
             ?><!DOCTYPE HTML>
             <html>
             <head>
@@ -135,7 +126,7 @@
                       window.onload = function () {
                            var chart = new CanvasJS.Chart("chartContainer", {
                                 title: {
-                                     text: "Laporan Perolehan Panen <?php echo $tahun;?>"
+                                     text: "Laporan Pemesanan <?php echo $tahun;?>"
                                 },
                                 legend: {
                                      cursor: "pointer",
@@ -167,7 +158,43 @@
                                           { x: new Date(<?php echo $tahun;?>, 10, 1), y: <?php echo $id11;?> },
                                           { x: new Date(<?php echo $tahun;?>, 11, 1), y: <?php echo $id12;?> }
                                      ]
-                                }]
+                                }/*, {
+                                     showInLegend: true,
+                                     type: "line",
+                                     name:"Jagung",
+                                     dataPoints: [
+                                          { x: new Date(<?php //echo $tahun;?>, 0, 1), y: <?php //echo $idl1;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 1, 1), y: <?php //echo $idl2;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 2, 1), y: <?php //echo $idl3;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 3, 1), y: <?php //echo $idl4;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 4, 1), y: <?php //echo $idl5;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 5, 1), y: <?php //echo $idl6;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 6, 1), y: <?php //echo $idl7;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 7, 1), y: <?php //echo $idl8;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 8, 1), y: <?php //echo $idl9;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 9, 1), y: <?php //echo $idl10;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 10, 1), y: <?php// echo $idl11;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 11, 1), y: <?php// echo $idl12;?> }
+                                     ]
+                                }/*, {
+                                     showInLegend: true,
+                                     type: "line",
+                                     name:"Laporan Pembayaran",
+                                     dataPoints: [
+                                          { x: new Date(<?php //echo $tahun;?>, 0, 1), y: <?php //echo $idll1;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 1, 1), y: <?php //echo $idll2;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 2, 1), y: <?php //echo $idll3;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 3, 1), y: <?php //echo $idll4;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 4, 1), y: <?php //echo $idll5;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 5, 1), y: <?php //echo $idll6;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 6, 1), y: <?php //echo $idll7;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 7, 1), y: <?php //echo $idll8;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 8, 1), y: <?php //echo $idll9;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 9, 1), y: <?php //echo $idll10;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 10, 1), y: <?php //echo $idll11;?> },
+                                          { x: new Date(<?php //echo $tahun;?>, 11, 1), y: <?php //echo $idll12;?> }
+                                     ]
+                                }*/]
                            });
 
                            chart.render();
