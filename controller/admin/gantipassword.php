ac8fc55abe280ffbfe57876e7cd1c75375114073
afiffarishudaifi <afiffaris5@gmail.com>
2020-01-30 10:47:01 +0700

    pengaturan admin

--- controller/admin/gantipassword.php
+++ controller/admin/gantipassword.php
@@ -3,6 +3,7 @@ include "../koneksi.php";
 require_once "../session.php";
 session_start();
 if (isset($login_session)) {
+
     $userid =  $user_check;
 } else {
     die("Error. No ID Selected!");
@@ -14,34 +15,27 @@ if (isset($_POST['Ganti'])) {
     $pass_lama = $_POST['pass_lama'];
     $pass_baru = $_POST['pass_baru'];
     $pass_konf = $_POST['pass_konf'];
-    $foto = $_FILES['foto']['name'];
-    $tmp = $_FILES['foto']['tmp_name'];
 
-    //merename foto dengan menambah tgl dan jam upload
-    $fotobaru = $foto;
-    $fotobaru = $gambar.".jpg";
-    //set path folder tempat menyimpan foto
-    $path = "../../img/user/".$fotobaru;
     //Cek Password Lama
-    $query = "SELECT * FROM USER WHERE ID_USER='$userid' AND password='$pass_lama'";
+    $query = "SELECT * FROM USER WHERE ID_USER='$userid' AND password=md5('$pass_lama')";
     $sql = mysqli_query($koneksi, $query);
     $hasil = mysqli_num_rows($sql);
     if (!$hasil >= 1) {
     ?>
         <script language="JavaScript">
         alert('Password lama tidak sesuai, Silahkan ulang kembali !');
-        setTimeout(function() {window.location.href='../../pages/user/pengaturan'},10);
+        setTimeout(function() {window.location.href='../../pages/admin/pengaturan'},10);
         </script>
     <?php
         // unset($_SESSION['userid']);
         // session_destroy();
     }
     //Validasi data data kosong
-    elseif (empty($_POST['pass_baru']) || empty($_POST['pass_konf'])) {
+    else if (empty($_POST['pass_baru']) || empty($_POST['pass_konf'])) {
         ?>
         <script language="JavaScript">
         alert('Gagal ganti password, Data tidak boleh kosong !');
-        setTimeout(function() {window.location.href='../../pages/user/pengaturan'},10);
+        setTimeou t(function() {window.location.href='../../pages/admin/pengaturan'},10);
         </script>
     <?php
 
@@ -51,37 +45,31 @@ if (isset($_POST['Ganti'])) {
         ?>
         <script language="JavaScript">
         alert('Gagal Ganti Password,New Password harus sama dengan Confirm Password !');
-        setTimeout(function() {window.location.href='../../pages/user/pengaturan'},10);
+        setTimeout(function() {window.location.href='../../pages/admin/pengaturan'},10);
         </script>
     <?php
     }
     //Update data
-    else {
-        if (file_exists($gambar)) {
-            unlink($gambar); //hapus
-        } else {
-            if(move_uploaded_file($tmp, $path)){
-                $query = "UPDATE user SET password = '$pass_baru', FOTO_USER = '$fotobaru' WHERE ID_USER='$userid'";
-                $sql = mysqli_query($koneksi,$query);
-                //Setelah diupdate
-                if($sql){
-                    ?>
-                    <script language="JavaScript">
-                    alert('Password Berhasil di Update!');
-                    setTimeout(function() {window.location.href='../../pages/user/pengaturan'},10);
-                    </script>
-                <?php
-                    }
-                else{
-                    ?>
-                    <script language="JavaScript">
-                    alert('Password Gagal di Update !');
-                    </script>
-                <?php
-
-                    }
+    else{
+        $query = "UPDATE user SET password = md5('$pass_baru') WHERE ID_USER='$userid'";
+        $sql = mysqli_query($koneksi,$query);
+        //Setelah diupdate
+        if($sql){
+            ?>
+            <script language="JavaScript">
+            alert('Password Berhasil di Update!');
+            setTimeout(function() {window.location.href='../../pages/admin/pengaturan'},10);
+            </script>
+        <?php
             }
-        }
+        else{
+            ?>
+            <script language="JavaScript">
+            alert('Password Gagal di Update !');
+            </script>
+        <?php
+
+            }
     }
 }
 
